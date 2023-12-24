7c08df2aa01bbd29fea00e336e00666c323822d1
paulomarchon <paulomarchon@gmail.com>
2023-12-24 19:19:58 -0300

    implementation of public and private key converters

--- auth-server/src/main/java/com/paulomarchon/authserver/keys/RsaPrivateKeyConverter.java
+++ auth-server/src/main/java/com/paulomarchon/authserver/keys/RsaPrivateKeyConverter.java
@@ -4,11 +4,16 @@ import org.springframework.core.serializer.Deserializer;
 import org.springframework.core.serializer.Serializer;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 
 @Component
 public class RsaPrivateKeyConverter implements Serializer<RSAPrivateKey>, Deserializer<RSAPrivateKey> {
@@ -20,11 +25,26 @@ public class RsaPrivateKeyConverter implements Serializer<RSAPrivateKey>, Deseri
 
     @Override
     public RSAPrivateKey deserialize(InputStream inputStream) throws IOException {
-    return null; //TODO
+        try {
+            String pem = this.textEncryptor.decrypt(FileCopyUtils.copyToString(new InputStreamReader(inputStream)));
+            String privateKeyPem = pem
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "");
+            byte[] encoded = Base64.getMimeDecoder().decode(privateKeyPem);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+            return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+        } catch (Throwable throwable) {
+            throw new IllegalArgumentException("error when trying to deserialize the private key", throwable);
+        }
     }
 
     @Override
-    public void serialize(RSAPrivateKey object, OutputStream outputStream) throws IOException {
-
+    public void serialize(RSAPrivateKey key, OutputStream outputStream) throws IOException {
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(key.getEncoded());
+        String str = "-----BEGIN PRIVATE KEY-----\n" +
+                     Base64.getMimeEncoder().encodeToString(pkcs8EncodedKeySpec.getEncoded())
+                     + "\n-----END PRIVATE KEY-----";
+        outputStream.write(this.textEncryptor.encrypt(str).getBytes());
     }
 }
